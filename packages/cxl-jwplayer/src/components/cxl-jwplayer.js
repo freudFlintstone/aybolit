82151617285d25793888f9aac474dd3c14e8650d
Raphael Mattos <raphael.mattos@contextaware.com.br>
2020-10-01 10:35:36 -0300

    feat(cxl-jwplayer): simpler setup, using external config object. move styles to global

--- packages/cxl-jwplayer/src/components/cxl-jwplayer.js
+++ packages/cxl-jwplayer/src/components/cxl-jwplayer.js
@@ -1,15 +1,10 @@
 import { LitElement, html, customElement, property } from 'lit-element';
 import '@conversionxl/cxl-lumo-styles';
-// import { registerGlobalStyles } from '@conversionxl/cxl-lumo-styles/src/utils';
-import cxlJwplayerStyles from '../styles/cxl-jwplayer-css.js';
-// import cxlJwplayerGlobalStyles from '../styles/global/cxl-jwplayer-css.js';
+import { registerGlobalStyles } from '@conversionxl/cxl-lumo-styles/src/utils';
+import cxlJwplayerGlobalStyles from '../styles/global/cxl-jwplayer-css.js';
 
 @customElement('cxl-jwplayer')
 export class CxlJwPlayer extends LitElement {
-  static get styles() {
-    return [cxlJwplayerStyles];
-  }
-
   @property({ type: Object })
   player = null;
 
@@ -22,6 +17,7 @@ export class CxlJwPlayer extends LitElement {
   constructor() {
     super();
     this.jwplayer = window.jwplayer || null;
+    // eslint-disable-next-line no-console
     if (!this.jwplayer) console.error('JWPlayer library not found.');
   }
 
@@ -34,12 +30,11 @@ export class CxlJwPlayer extends LitElement {
 
   render() {
     return html`
-      <h2>cxl-jwplayer</h2>
-      <div id="cxl-jwplayer-container" class="cxl-jwplayer-container">
-        <div id="cxl-jwplayer" class="cxl-jwplayer jwplayer"></div>
+      <div id="cxl_transcript_container_${this.mediaId}" class="cxl-jwplayer-container">
+        <div id="cxl_jwplayer_${this.mediaId}" class="cxl-jwplayer"></div>
         ${this.config.transcript
           ? html`
-              <div id="cxl-transcript-container" class="transcript-container">
+              <div id="cxl_transcript_container" class="transcript-container">
                 <div id="searchbox" class="searchbox">
                   <input
                     id="search"
@@ -53,7 +48,7 @@ export class CxlJwPlayer extends LitElement {
                 </div>
                 <div class="jw-aspect jw-reset"></div>
                 <div class="transcript-wrapper">
-                  <div id="transcript" class="transcript"></div>
+                  <div id="transcript_${this.mediaId}" class="transcript"></div>
                 </div>
               </div>
             `
@@ -68,85 +63,26 @@ export class CxlJwPlayer extends LitElement {
     if (!this.jwplayer || !this.mediaId) return;
 
     this.setupPlayer();
+
+    registerGlobalStyles(cxlJwplayerGlobalStyles, {
+      moduleId: 'cxl-jwplayer-global'
+    });
   }
 
   setupPlayer() {
     /**
      *
-     * Configure and Setup the Player
+     * Configure and Setup the Player using config object assigned by parent
      *
      */
-    // Fetched via GET request to JW Platform
-    // One video is an object. Multiple videos is an array of objects.
-    // Paths are hard coded for now, while developing this implementation.
-    const data = {
-      config: {
-        playlist: [
-          {
-            file: `https://cdn.jwplayer.com/manifests/${this.mediaId}.m3u8`,
-            image: `https://cdn.jwplayer.com/thumbs/${this.mediaId}-720.jpg`,
-            tracks: [
-              {
-                file: `https://cdn.jwplayer.com/strips/${this.mediaId}-120.vtt`,
-                kind: 'thumbnails'
-              },
-              {
-                file: 'https://assets-jpcust.jwpsrv.com/tracks/HujCQO8E',
-                kind: 'captions',
-                label: 'English',
-                language: 'en_US'
-              },
-              {
-                file: 'https://content.jwplatform.com/tracks/qb36nsWb.vtt', // need to be able to set these dynamically...
-                kind: 'chapters'
-              }
-            ]
-          }
-        ]
-      }
-    };
+    if (!Object.keys(this.config).length || !this.config.media_config) {
+      // eslint-disable-next-line no-console
+      console.warn('No media config data for player setup.');
+    }
 
-    const { config } = data;
-    config.plugins = {};
+    if (!this.mediaId) this.mediaId = this.config.media_config.playlist[0].mediaId;
 
-    /**
-     *
-     * Conditionally load plugins
-     *
-     */
-    // let hasChapters = false;
-    // let hasCaptions = false;
-    // const { tracks } = config.playlist[0];
-
-    // tracks.forEach(track => {
-    //   if (track.kind.startsWith('chapters')) {
-    //     hasChapters = true;
-    //   }
-    //   if (track.kind.startsWith('captions')) {
-    //     hasCaptions = true;
-    //   }
-    // });
-
-    // How these are loaded in php is different. We do the plugin name = true
-    // We use WP to enqueue the scripts, but here we have to init from the jw setup config.
-    // EX: config.plugins['cxlJWChapters'] = true.
-    // if (hasChapters) {
-    //   config.plugins['../plugins/cxlJWChapters.js'] = 'cxlJWChapters';
-    // }
-    // if (hasCaptions) {
-    //   config.plugins['../plugins/cxlJWTranscript.js'] = 'cxlJWTranscript';
-    // }
-
-    config.controls = true;
-    config.playbackRateControls = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
-    config.displaydescription = false;
-
-    config.skin = {
-      name: 'cxl-institute'
-    };
-
-    const playerDiv = this.querySelector('#cxl-jwplayer');
-    this.config = config;
-    this.player = this.jwplayer(playerDiv).setup(config);
+    const playerDiv = this.querySelector(`#cxl_jwplayer_${this.mediaId}`);
+    this.player = this.jwplayer(playerDiv).setup(this.config.media_config);
   }
 }
